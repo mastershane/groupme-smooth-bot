cbf55f86205ad89ea825f854aa8d53eb6bcabfb4
mastershane <shanenotsean@hotmail.com>
2025-04-09 22:42:23 -0700

    Adding more rob thomas data

--- src/rob-thomas-data.ts
+++ src/rob-thomas-data.ts
@@ -53,7 +53,44 @@ Rob Thomas's mom was diagnosed with cancer when he was 12 years old
 Rob Thomas's sister ranaway and got married at 17.
 Mari Punk'd rob to see if he really loved her ♥
 Rob wore Gucci to his wedding and Mari wore Vera Wang
-Marisol knew she was going to marry Rob after the first time they met`
+Marisol knew she was going to marry Rob after the first time they met
+Rob doesnt wear underwear(;
+Rob & Santana won 6 Grammy's in one night for "Smooth"
+Rob has a Blackberry Phone.
+Rob wrote ''Ever The Same'' about Mari, And one night she fell asleep crying in his lap,because the doctors couldnt find a diagnosis.
+''3am'' is the very first song Rob ever write that he liked,and is about his mom battling with cancer.
+Rob wrote ''Smooth'' about Marisol.
+Rob wrote "Rest Stop" about an Ex girlfriend who was taking him to Daytona,and just pulled over and told him to get out-said it wasnt workng.
+Rob wrote "Her Diamonds" about being strong for Mari.
+Rob made his twitter account on March 11, 2009.
+Rob tweeted ME for the first time on September 02, 2011 <3
+Rob joined choir because of a girl he liked.
+Rob took his music book to parties,so when the 'jocks' would get drunk and pass out,he would sing for their girlfriends.
+Him and Mari have a "No Reality" rule in their home.
+Rob went to Maui for his honeymoon.
+Rob has 2 little freckles on the left side of his neck.
+Rob used to do drugs.
+Rob doesnt remeber most of the beginning of his career because of all the drungs he did.
+Rob smokes cigarettes- his favorite are Camel Light.
+The hughschool he went to is called Lake Brantely High School.
+Rob's bestfriend is Paul Doucette
+Rob played football and surfed in highschool.
+Him and Mari have a charity for animals called Sidewalk Angels.
+Rob knew he would spend the rest of his life with Mari after their first kiss.
+Rob got super scared when he had to stand over the edge in the "If You're Gone" video.He said it didnt matter that he had straps on,it still freaked him out.
+Rob wrote If You're Gone about Mari- One day she woke up and said she couldnt marry a rockstar,it just wouldnt work. so he wrote that song for her
+Rob proposed to Mari in a parking lot while they were in the middle of an arguement/ He asked her "Do you love me?" And she said "I will love you for the rest of my life" And he proposed <3
+Rob's penis sits on the right side of his pants.
+Mari has an auto immune disease similar to lupus, but is in remission.
+Rob said if he was a city, he'd be New York City.
+Rob said if he was a festival he's be Bonaru.
+Rob said if he was a movie, he's be along the lines of Walk The Line, Basquiat & Charlie & the Chocolate Factory.
+If Rob was a dog he said he'd be a Golden Retriever
+If Rob was a website,He'd want to be Google,but would probably be robthomas.com
+If Rob was a color he would want to be along the lines of Navy Blue and Black
+If he was an instrument, he said he definitely be an acoustic guitar.
+Rob & Mari own two homes in New York- one in Katonah, & one in Westchester.
+`
 
 
 export const robDataArray = robData.split('\n')
